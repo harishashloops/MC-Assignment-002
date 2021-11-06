7f157ab99a0674376a9463dd891b7ebac6dbfb73
Haris Khalid <hariskhalid@hashloops.com>
2021-11-06 16:21:32 +0500

    boiler plate

--- app/src/main/java/com/devloper_haris/mc_assignment_002/HomeScreen.java
+++ app/src/main/java/com/devloper_haris/mc_assignment_002/HomeScreen.java
@@ -1,8 +1,13 @@
 package com.devloper_haris.mc_assignment_002;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static android.content.Intent.ACTION_VIEW;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -10,5 +15,39 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String email = intent.getStringExtra("email");
+
+        // Capture the layout's TextView and set the string as its text
+        TextView nameView = findViewById(R.id.nameView);
+        nameView.setText("welcome "+name);
+
+//        TextView emailView = findViewById(R.id.emailView);
+//        emailView.setText(email);
+    }
+    public void sendMessage(View view) {
+//        Log.d("first log","hello");
+//        Intent intent = new Intent(this, HomeScreen.class);
+//        EditText editText = (EditText) findViewById(R.id.Name);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+
+        // Calling
+//        Uri uri= Uri.parse("tel:=923014298840");
+//        Intent intent=new Intent(ACTION_DIAL,uri);
+//        startActivity(intent);
+
+        //open web page
+        openWebPage("https://www.google.com/");
+
+    }
+
+    public void openWebPage(String url)
+    {
+        Uri webpage=Uri.parse(url);
+        Intent intent =new Intent(ACTION_VIEW,webpage);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/devloper_haris/mc_assignment_002/MainActivity.java
+++ app/src/main/java/com/devloper_haris/mc_assignment_002/MainActivity.java
@@ -2,12 +2,19 @@ package com.devloper_haris.mc_assignment_002;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import java.net.URI;
+
+import static android.content.Intent.*;
+
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +22,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view) {
-        Log.d("first log","hello");
-        // Do something in response to button
+
+        Intent intent = new Intent(this, HomeScreen.class);
+        EditText nameText = (EditText) findViewById(R.id.Name);
+        EditText emailText = (EditText) findViewById(R.id.Email);
+
+        String Name = nameText.getText().toString();
+        String Email = emailText.getText().toString();
+        intent.putExtra("name", Name);
+        intent.putExtra("email", Email);
+        startActivity(intent);
+
+        // Calling
+//        Uri uri= Uri.parse("tel:=923014298840");
+//        Intent intent=new Intent(ACTION_DIAL,uri);
+//        startActivity(intent);
+
+
     }
+
+
 }

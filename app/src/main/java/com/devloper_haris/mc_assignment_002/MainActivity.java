fd96e0d46d88528a34b77fa9648d14f957c39b9f
Haris Khalid <hariskhalid@hashloops.com>
2021-11-06 15:26:26 +0500

    2nd commit

---
+++ app/src/main/java/com/devloper_haris/mc_assignment_002/MainActivity.java
@@ -0,0 +1,21 @@
+package com.devloper_haris.mc_assignment_002;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Log.d("first log","hello");
+        // Do something in response to button
+    }
+}
